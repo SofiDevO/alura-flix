651ec997ce8bdf615c3b580bfa4e5ee29ccfd64e
SofiDevO <angiemeow89@gmail.com>
2023-09-27 11:18:18 -0600

    update

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Crea una instancia de axios con la base de la API
 export const api = axios.create({
-  baseURL: "http://localhost:5555/",
-  /* usa este url pra hacer pruebas CRUD http://localhost:5555/    
+  baseURL: "https://fake-api-aluraflix-two.vercel.app",
+  /* usa este url pra hacer pruebas CRUD http://localhost:5555/  ,  
   https://fake-api-aluraflix-two.vercel.app/
   */
 });
